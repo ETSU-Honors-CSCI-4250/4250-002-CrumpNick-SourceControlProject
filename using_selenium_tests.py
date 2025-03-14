6a2ca4905804ff350e7fc7fde5f2a6c1658a2c22
Nicholas Crump <113931857+Kataruse@users.noreply.github.com>
2025-03-14 13:11:14 -0400

    Update using_selenium_tests.py

--- using_selenium_tests.py
+++ using_selenium_tests.py
@@ -1,7 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+driver = webdriver.Chrome()
+driver.get("http://localhost:8000")
 
+driver = setup()
+
+title = driver.title
+assert title == "4250 Honors Project"
+        
+driver.implicitly_wait(0.5)
+
+s1_button = driver.find_element(by=By.ID, value="s1")
+s2_button = driver.find_element(by=By.ID, value="s2")
+    
+s1_button.click()
+bg_color_s1 = driver.execute_script("return window.getComputedStyle(document.body).backgroundColor;")
+s2_button.click()
+tx_color_s2 = driver.execute_script("return window.return window.getComputedStyle(arguments[0]).color;", h1_element)
+    
+assert bg_color_s1 == "rgb(144, 238, 144)"
+assert tx_color_s2 == "rgb(255, 0, 0)"
+
+teardown(driver)
+
+driver.quit()
+
+'''
 def test_two_buttons():
     driver = setup()
 
@@ -30,3 +55,4 @@ def setup():
 
 def teardown(driver):
     driver.quit()
+'''
