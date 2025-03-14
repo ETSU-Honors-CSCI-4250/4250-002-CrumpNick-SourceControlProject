0b67c6a74a932a26447acd1f8c7446c3d5188310
Nicholas Crump <113931857+Kataruse@users.noreply.github.com>
2025-03-14 12:28:39 -0400

    Update using_selenium_tests.py

--- using_selenium_tests.py
+++ using_selenium_tests.py
@@ -1,3 +1,32 @@
+'''
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("../simple.html")
+
+title = driver.title
+
+driver.implicitly_wait(1)
+
+
+s1_button = driver.find_element(by=By.ID, value="s1")
+s1_button.click()
+
+bg_color_s1 = driver.execute_script("return window.getComputedStyle(document.body).backgroundColor;")
+assert bg_color_s1 == "rgb(144, 238, 144)", f"S1 background mismatch! Found: {bg_color_s1}"
+
+s2_button = driver.find_element(by=By.ID, value="s2")
+s2_button.click()
+
+bg_color_s2 = driver.execute_script("return window.getComputedStyle(document.body).backgroundColor;")
+assert bg_color_s2 == "rgb(255, 0, 0)", f"S2 background mismatch! Found: {bg_color_s2}"
+
+print("All tests passed successfully!")
+driver.quit()
+'''
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -7,24 +36,43 @@ def test_eight_components():
 
     title = driver.title
     assert title == "Web form"
+    # assert title == "4250 Honors Project"
 
     driver.implicitly_wait(0.5)
 
     text_box = driver.find_element(by=By.NAME, value="my-text")
     submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-
+    # s1_button = driver.find_element(by=By.ID, value="s1")
+    # s2_button = driver.find_element(by=By.ID, value="s2")
+    
+    '''
+    s1_button.click()
+    
+    bg_color_s1 = driver.execute_script("return window.getComputedStyle(document.body).backgroundColor;")
+    assert bg_color_s1 == "rgb(144, 238, 144)", f"S1 background mismatch! Found: {bg_color_s1}"
+    
+    s2_button.click()
+    '''
+    
     text_box.send_keys("Selenium")
     submit_button.click()
-
+    # s1_button.click()
+    # bg_color_s1 = driver.execute_script("return window.getComputedStyle(document.body).backgroundColor;")
+    # s2_button.click()
+    #bg_color_s2 = driver.execute_script("return window.getComputedStyle(document.body).backgroundColor;")
+    
     message = driver.find_element(by=By.ID, value="message")
     value = message.text
     assert value == "Received!"
+    # assert bg_color_s1 == "rgb(144, 238, 144)", f"S1 background mismatch! Found: {bg_color_s1}"
+    # assert bg_color_s2 == "rgb(255, 0, 0)", f"S2 background mismatch! Found: {bg_color_s2}"
 
     teardown(driver)
 
 def setup():
     driver = webdriver.Chrome()
     driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+    # driver.get("http://localhost:8000")
     return driver
 
 def teardown(driver):
