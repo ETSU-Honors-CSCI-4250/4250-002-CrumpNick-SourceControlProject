69c212982971943d7a2a778b73d39e1ff1461ef8
Nicholas Crump <113931857+Kataruse@users.noreply.github.com>
2025-03-14 13:08:30 -0400

    Update using_selenium_tests.py

--- using_selenium_tests.py
+++ using_selenium_tests.py
@@ -7,9 +7,7 @@ def test_two_buttons():
 
     title = driver.title
     assert title == "4250 Honors Project"
-    
-    print(f"Current page URL: {driver.current_url}")
-    
+        
     driver.implicitly_wait(0.5)
 
     s1_button = driver.find_element(by=By.ID, value="s1")
@@ -19,11 +17,9 @@ def test_two_buttons():
     bg_color_s1 = driver.execute_script("return window.getComputedStyle(document.body).backgroundColor;")
     s2_button.click()
     tx_color_s2 = driver.execute_script("return window.return window.getComputedStyle(arguments[0]).color;", h1_element)
-    #rgb(144, 238, 144)
-    print(f"S1 background color: {bg_color_s1}")
-    print(f"S2 text color: {tx_color_s2}")
-    assert bg_color_s1 == "rgb(0, 0, 0)"
-    assert tx_color_s2 == "rgb(0, 0, 0)"
+    
+    assert bg_color_s1 == "rgb(144, 238, 144)"
+    assert tx_color_s2 == "rgb(255, 0, 0)"
 
     teardown(driver)
 
